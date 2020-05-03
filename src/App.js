074b883685c46ee9e56d433c688bf7ac8d96449d
Suraj Shrivastava <srjshrivastv1@gmail.com>
2020-05-03 20:20:07 +0530

    Add Api

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ import BookShelf from './components/Bookshelf'
 import SearchPage from './components/Search'
 import OnGoing from './components/OnGoing'
 import Wish from './components/WishList'
+import * as BookApi from './BooksAPI'
+
+
+// function ShiftTo(To,from){
+
+// }
+
 
 class BooksApp extends React.Component {
+  state={
+    'CurrentlyReading':{},
+    'Read':{},
+    'WantToRead':{}
+
+  }
+  componentDidMount(){
+    BookApi.getAll().then(
+      (result)=>{
+        this.setState(()=>({ 
+          'CurrentlyReading':result.filter((b)=>b.shelf==='currentlyReading'),
+          'Read':result.filter((b)=>b.shelf==='read'),
+          'WantToRead':result.filter((b)=>b.shelf==='wantToRead'),
+          }))
+        })
+  }
   render() {
     return (
       <div className="app">
@@ -19,9 +42,9 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <OnGoing />
-                <Wish/>
-                <BookShelf />
+                <OnGoing books={this.state.CurrentlyReading} />
+                <Wish books={this.state.WantToRead}/>
+                <BookShelf books={this.state.Read}/>
               </div>
             </div>
             <div className="open-search">
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,6 +1,8 @@
 import React ,{Component} from 'react'
 
+
 class BookShelf extends Component{
+
 
     render(){
         return(
@@ -13,7 +15,7 @@ class BookShelf extends Component{
                     <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
                       <div className="book-shelf-changer">
-                        <select>
+                        <select  id='move'>
                           <option value="move" disabled>Move to...</option>
                           <option value="currentlyReading">Currently Reading</option>
                           <option value="wantToRead">Want to Read</option>
