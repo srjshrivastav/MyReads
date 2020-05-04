faa9174cd5a795b238f42c2af5cbccc206f3969a
Suraj Shrivastava <srjshrivastv1@gmail.com>
2020-05-04 15:20:07 +0530

    Bookshelf Is now functioning Properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Route, Link} from 'react-router-dom'
 import BookShelf from './components/Bookshelf'
@@ -9,13 +8,39 @@ import * as BookApi from './BooksAPI'
 
 class BooksApp extends React.Component {
   state={
-    'CurrentlyReading':[],
-    'Read':[],
-    'WantToRead':[]
+    currentlyReading:[],
+    read:[],
+    wantToRead:[]
 
   }
-  ShiftTo=(book,shelf)=>{
-    console.log(book.id,shelf)
+  ShiftTo=(book,s)=>{
+    let curr=book.shelf;
+    let b =this.state[curr].splice(this.state[curr].indexOf(this.state[curr].filter((b)=>b.id===book.id)[0]),1)
+    b[0].shelf=s
+    console.log(s)
+    switch(s){
+      case 'currentlyReading':
+        this.setState((state)=>({
+          currentlyReading:[...state.currentlyReading,b[0]]
+    
+        }))
+        break;
+      case 'wantToRead':
+        this.setState((state)=>({
+          wantToRead:[...state.wantToRead,b[0]]
+    
+        }))
+        break; 
+      case 'read':
+        this.setState((state)=>({
+          read:[...state.read,b[0]]
+    
+        }))
+        break; 
+      default:
+        return this.state
+    }
+BookApi.update(book,s)
   }
   
 
@@ -23,13 +48,14 @@ class BooksApp extends React.Component {
     BookApi.getAll().then(
       (result)=>{
         this.setState(()=>({ 
-          'CurrentlyReading':result.filter((b)=>b.shelf==='currentlyReading'),
-          'Read':result.filter((b)=>b.shelf==='read'),
-          'WantToRead':result.filter((b)=>b.shelf==='wantToRead'),
+          currentlyReading:result.filter((b)=>b.shelf==='currentlyReading'),
+          read:result.filter((b)=>b.shelf==='read'),
+          wantToRead:result.filter((b)=>b.shelf==='wantToRead'),
           }))
         })
   }
   render() {
+
     return (
       <div className="app">
         <Route exact path='/search' component={SearchPage} />
@@ -40,13 +66,13 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <BookShelf books={this.state.CurrentlyReading}
+                <BookShelf books={this.state.currentlyReading}
                 Move={this.ShiftTo}
                 shelf={'Currently Reading'} />
-                <BookShelf books={this.state.WantToRead}
+                <BookShelf books={this.state.wantToRead}
                 Move={this.ShiftTo} 
                 shelf={'Want To Read'}/>
-                <BookShelf books={this.state.Read}
+                <BookShelf books={this.state.read}
                 Move={this.ShiftTo}
                 shelf={'Read'} />
               </div>
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,31 +1,37 @@
 import React ,{Component} from 'react'
 
 
+
 class BookShelf extends Component{
+  handleevent=(event,book)=>{
+  if(event.target.value!=='none')
+    this.props.Move(book,event.target.value)
+  }
 
 
     render(){
+      const books =this.props.books
         return(
             <div className="bookshelf">
             <h2 className="bookshelf-title">{this.props.shelf}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                {this.props.books.map((book)=>(
+                {books.map((book)=>(
                   <li key={book.id}>
                   <div className="book">
                     <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
                       <div className="book-shelf-changer">
-                        <select id='select'>
+                        <select id='select' onChange={(event)=>{this.handleevent(event,book)}} value={book.shelf}>
                           <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
+                          <option value="currentlyReading" disabled={book.shelf==='currentlyReading'?true:false}>Currently Reading</option>
+                          <option value="wantToRead" disabled={book.shelf==='wantToRead'?true:false}>Want to Read</option>
+                          <option value="read" disabled={book.shelf==='read'?true:false}>Read</option>
                           <option value="none">None</option>
                         </select>
                       </div>
                     </div>
-                    <div className="book-title">book.title</div>
+                    <div className="book-title">{book.title}</div>
                     <div className="book-authors">{book.authors.join(', ')}</div>
                   </div>
                 </li>
